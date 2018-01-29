892e001698b29031adaf26e29b7453f028bc17ee
Eric Stone <estone4@gmail.com>
2018-01-28 18:27:20 -0600

    Working subtitle converter
    Corrects ISO-8859-9 (Turkish) character to utf-8 equivalent.  This allows characters that are not in the English alphabet to be displayed correctly.  There is no error checking to see if the conversion has already been done.  If ran against a file hat has already been converted it will change those characters to something unreadable.

---
+++ subtitle.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+"""
+Subtitle converter
+
+Corrects improper Turkish characters.
+"""
+
+#IMPORTS
+#--------------------------------------------------------
+#import glob
+#--------------------------------------------------------
+
+#VARIABLES
+#--------------------------------------------------------
+#fileDict = {}
+#fileContents = []
+data = ""
+#--------------------------------------------------------
+
+#MAIN PROGRAM
+#--------------------------------------------------------
+#for file in glob.glob('*.srt'):
+
+f=open("a-walk-to-remember_turkish-22705.srt", "r", encoding="ISO-8859-9")
+#print("Test")		#DEBUG PRINT
+data = f.read()
+f.close()
+#print(data)		#DEBUG PRINT
+
+f=open("SubtitleTest.srt", "w", encoding="utf-8")
+f.write(data)
+f.close()
+#print(data)		#DEBUG PRINT
+
+#print(type(data))	#DEBUG PRINT
+#--------------------------------------------------------
