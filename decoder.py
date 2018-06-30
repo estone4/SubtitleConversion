789d4f94679aa5cd8d136436a38cce2293f06bc4
Eric Stone <estone4@gmail.com>
2018-06-29 23:00:53 -0500

    Prototype "bitwise AND" to check if the bytes are already unicode.

--- decoder.py
+++ decoder.py
@@ -5,13 +5,32 @@ raw_bytes = input_file.read()
 
 print(raw_bytes)
 
-try:
-    decoded_bytes = str(raw_bytes.decode('utf-8'))
-except Exception as e:
-    print('Error decoding file as "UTF-8"!\n')
-    print(e)
-    print('Decoding file as "ISO-8859-9"\n')
-    decoded_bytes = str(raw_bytes.decode('iso-8859-9'))
+unicode_byte_count = 0
+unicode_index = []
+mask = 0x80
 
-if decoded_bytes is not None:
-    print(decoded_bytes)
+for byte in raw_bytes:
+    if mask & byte:
+        print("high bit set for %c" % byte)
+        if mask >> 1 & byte:
+            print("Found 2nd high bit")
+            if mask >> 2 & byte:
+                print("3rd high bit set")
+            else:
+                print("3rd high bit NOT set")
+        unicode_byte_count += 1
+        unicode_index.append(byte)
+
+print(unicode_byte_count)
+# print(unicode_index)
+
+# try:
+#     decoded_bytes = str(raw_bytes.decode('utf-8'))
+# except Exception as e:
+#     print('Error decoding file as "UTF-8"!\n')
+#     print(e)
+#     print('Decoding file as "ISO-8859-9"\n')
+#     decoded_bytes = str(raw_bytes.decode('iso-8859-9'))
+#
+# if decoded_bytes is not None:
+#     print(decoded_bytes)
