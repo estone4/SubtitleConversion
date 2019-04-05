f1df66ac35f8d08d17201809447895ff54a2400b
Eric Stone <estone4@gmail.com>
2019-04-04 21:05:58 -0500

    Changes

--- decoder.py
+++ decoder.py
@@ -1,28 +1,44 @@
 import codecs
+import os
 
-input_file = open('A.Walk.to.Remember.2002.720p.BluRay.x264.YIFY 2.srt', 'rb')
-raw_bytes = input_file.read()
+#print(os.sys.platform)
 
-print(raw_bytes)
+input_file = open('Test2.srt', 'rb')
+byte = input_file.read(1).decode()
+
+mask = b"0x80"
+while byte:
+    if byte & mask.decode():
+        print("High bit set for {}.".format(byte))
+
+#print(raw_bytes)
 
 unicode_byte_count = 0
 unicode_index = []
-mask = 0x80
+byte_index = 0  # Counts the number of bytes read to tack the location
+
+
+# need to figure out how to check each byte to s
 
 for byte in raw_bytes:
-    if mask & byte:
-        print("high bit set for %c" % byte)
-        if mask >> 1 & byte:
-            print("Found 2nd high bit")
-            if mask >> 2 & byte:
+    print("Byte #: {0}".format(byte))
+    byte_index += 1
+    if mask & byte:  # checks if the byte starts with the left most bit (bit 7) set  (This should indicate unicode)
+        print("high bit set for {0} at pos: {1}".format(byte, byte_index))
+        if mask >> 1 & byte:  # should be checking if bit 6 is also set
+            # (This lets us know how many remaining bytes to look for the rest of the unicode
+            print("Found 2nd high bit for %c at pos: %d" % (byte, byte_index))
+            if mask >> 2 & byte:  # should be checking if bit 5 is set
                 print("3rd high bit set")
             else:
                 print("3rd high bit NOT set")
         unicode_byte_count += 1
-        unicode_index.append(byte)
+        unicode_index.append(byte_index)
 
 print(unicode_byte_count)
+print(unicode_index)
 # print(unicode_index)
+print("Byte index: %d" % byte_index)
 
 # try:
 #     decoded_bytes = str(raw_bytes.decode('utf-8'))
@@ -34,3 +50,9 @@ print(unicode_byte_count)
 #
 # if decoded_bytes is not None:
 #     print(decoded_bytes)
+
+
+if byte_index > len(raw_bytes):
+    print("ERROR Reading file")
+else:
+    print(len(raw_bytes))
